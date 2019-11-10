34528f34e0ffde0d65a8b4d70a81df180932c64b
andreasblomqvist <adde-16@hotmail.com>
2019-11-10 20:35:42 +0100

    Labb 1

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace CalculatorX
+{
+    internal class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public int NumberInMemory { get; set; }
+
+        public Calculator()
+        {
+            FirstNumber = 0;
+            SecondNumber = 0;
+            NumberInMemory = 0;
+        }
+
+        public void calculatorMenu() {
+
+        }
+
+        public override string ToString()
+        {
+            return "First number: " + FirstNumber.ToString() +"\n"+
+                "Second number: " + SecondNumber.ToString() + "\n" +
+                "Number in memory: " + NumberInMemory.ToString();
+        }
+
+        public int SumNumbers() {
+            return FirstNumber + SecondNumber;
+        }
+
+        public int MultiplyNumbers() {
+            return FirstNumber * SecondNumber;
+        }
+
+        public int SumInBetween() {
+            int sum = 0;
+            for (int i = FirstNumber; i < SecondNumber; i++) {
+                sum = sum + i;
+            }
+            return sum;
+        }
+
+        public void enterNumOne() {
+            Console.WriteLine("Skriv in ett tal: ");
+            string numOne = Console.ReadLine();
+            FirstNumber = int.Parse(numOne);
+        }
+
+        public void enterNumTwo() {
+            Console.WriteLine("Skriv ditt andra tal: ");
+            string numTwo = Console.ReadLine();
+            SecondNumber = int.Parse(numTwo);
+        }
+
+        public void addNumToMemory() {
+            Console.WriteLine("Skriv in ett tal: ");
+            string num = Console.ReadLine();
+            NumberInMemory = int.Parse(num);
+            Console.WriteLine("Number successfully added to memory");
+        }
+
+        public void clearMemory() {
+            NumberInMemory = 0;
+            Console.WriteLine("Number in memory cleared");
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CalculatorX
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.FirstNumber = 5;
+            calc.SecondNumber = 10;
+            calc.NumberInMemory = 123;
+            bool programRunning = true;
+
+            while (programRunning) {
+                Console.WriteLine("1. Add two numbers\n" +
+                "2. Multiply two numbers\n" +
+                "3. Sum the numbers between two numbers\n" +
+                "4. Add number to memory\n" +
+                "5. Clear memory\n" +
+                "6. Print calculator contents\n" +
+                "7. Quit\n"
+                );
+                string selectedString = Console.ReadLine();
+                int selected = int.Parse(selectedString);
+
+                switch(selected) {
+                    case 1:
+                        calc.enterNumOne();
+                        calc.enterNumTwo();
+                        Console.WriteLine("Sum: " + calc.SumNumbers());
+                        break;
+                    case 2:
+                        calc.enterNumOne();
+                        calc.enterNumTwo();
+                        Console.WriteLine("Multiply: " + calc.MultiplyNumbers());
+                        break;
+                    case 3:
+                        calc.enterNumOne();
+                        calc.enterNumTwo();
+                        Console.WriteLine("Sum-Between: " + calc.SumInBetween());
+                        break;
+                    case 4:
+                        calc.addNumToMemory();
+                        break;
+                    case 5:
+                        calc.clearMemory();
+                        break;
+                    case 6:
+                        Console.WriteLine(calc.ToString());
+                        break;
+                    case 7:
+                        Console.WriteLine("Goodbye!");
+                        programRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Default");
+                        break;
+                }
+            }
+        }
+    }
+}
